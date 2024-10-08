f001b56a7d515423916bd1d250d86607d8d73f4a
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-10-08 11:08:34 +0100

    feat: add navigation buttons

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,18 @@ import Image from "next/image";
 
 // image the background image
 import bg from '../../public/background/home-background.png'
-import RenderModel from "./components/RenderModel";
-import Wizard from "./components/models/Wizard";
+import RenderModel from "../components/RenderModel";
+import Wizard from "../components/models/Wizard";
+import Navigation from "../components/navigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center relative">
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image src={bg} alt="background-image" fill className="-z-50 w-full h-full object-cover object-center opacity-25" />
 
-      <div className="flex justify-center w-full h-screen">
+      <div className="flex justify-center w-screen h-screen">
         {/* navigation and 3d model */}
+        <Navigation />
         <RenderModel>
           <Wizard />
         </RenderModel>
